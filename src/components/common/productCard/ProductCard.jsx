2529384b1047dd0c35fbd99ecd3f79bbb496918c
Minh Khanh <khanhdoan693@gmail.com>
2022-06-06 09:38:18 +0700

    update

--- src/components/common/productCard/ProductCard.jsx
+++ src/components/common/productCard/ProductCard.jsx
@@ -1,4 +1,4 @@
-import { Button, Card } from "antd";
+import { Button, Card, Image } from "antd";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./ProductCard.css";
@@ -10,12 +10,15 @@ export function ProductCard() {
       hoverable
       style={{ width: 200 }}
       cover={
-        <img
+        <Image
+          height={230}
+          preview={false}
+          style={{objectFit: "cover"}}
           onClick={() => {
             navigate("/ProjectThuVien/detail-page/1", { state: { id: 1 } });
           }}
           alt="book"
-          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
         />
       }
       actions={[
